08acd1bb923eac6a93534ce215a9bab7eef78de9
hwh <1319427512@qq.com>
2018-09-21 10:03:13 +0800

    idea中冲突测试

---
+++ src/test/java/TestCT.java
@@ -0,0 +1,12 @@
+/**
+ * idea冲突测试和解决
+ * @author hwh
+ * @create 2018/9/21 10:00
+ */
+public class TestCT {
+
+    //冲突测试
+    public void testCT() {
+        System.out.println("hello 111");
+    }
+}
